7bb33d45e836732cd440766a95778fd686c4af50
Sam Garson <samtgarson@gmail.com>
2015-04-27 17:49:18 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('browser-sync', function() {
       proxy: 'localhost:8080',
       open: false,
       minify: false,
-      files: ['build/script.js'],
+      files: ['*.html', 'build/script.js'],
       injectChanges: true
     });
 });
